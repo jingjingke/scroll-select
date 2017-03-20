4722035d6d79ca35933fd9c9bea40a785014eae8
jingjingke <919948650@qq.com>
2017-03-20 14:22:37 +0800

    日历三联选择原生JS初始化

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # scroll-select
 
-仿ios日期选择做的一个小插件（主分支依赖jquery，会抽时间制作纯原生代码分支）。
+仿ios日期选择做的一个小插件（当前为原生JS实现）。
 
-线上预览：[https://jingjingke.github.io/scroll-select/](https://jingjingke.github.io/scroll-select/)
+效果预览（主页为依赖JQ）：[https://jingjingke.github.io/scroll-select/](https://jingjingke.github.io/scroll-select/)
 
 
 ##目录结构
@@ -17,7 +17,6 @@
 ├── data
 │   └── city.json            // 省市区联动数据
 ├── js
-│   ├── jquery.min.js        // jquery依赖
 │   └── scroll-select.js     // 滚动效果js
 ├── index.html               // 效果预览
 ├── README.md                // readme
@@ -34,32 +33,18 @@ scrollSelect.go()
 ```js
 data	//[json]只省市区才需要传入data
 level   //[number](必需)联动等级（目前有1、2、3级）
-elArr   //[str](必需)传入表单的选择器名如#div或.div，用于获取默认值以及确定后将值添入
-type    //[str](必需)当前只支持省市区地址（address）和日期（calendar）
-el      //[str]这个按需来传非表单选择器，用于将默认值以某种格式添入
+type    //[str](必需)当前只支持省市区地址（address调整中还不能用）和日期（calendar）
+el      //[str][有区别]传递为Id选择器名
 ```
 
 
 ##示例
-（1）调用三级联动省市区选择，如果只需要省市则level改为2即可
+暂时只调整了日历的的代码，示例
 ```js
-$('.form_list1').on('click',function(){
-    $.getJSON("./data/city.json",function(result){
-        scrollSelect.go({
-            data:result,
-            level:3,
-            elArr:'.select1',
-            type:'address'
-        })
-    });
-})
-```
-（2）调用日期格式
-```js
-$('.form_list3').on('click',function(){
+document.getElementById('input3').addEventListener('click',function(e){
     scrollSelect.go({
         level:3,
-        elArr:'.input3',
+        el:'input3',
         type:'calendar'
     })
 })
--- js/scroll-select.js
+++ js/scroll-select.js
@@ -1,11 +1,25 @@
 var scrollSelect = {
 	//定义与初始值
-	_obj : {},         type:'',         data : {},       level: 0 ,      value:[],
-	tempIdx : [],      realIdx : [],    index : [],
-	dataObj : [],      dataArr : [],
-	warpList : [],     ulList : [],     liList : [],
-	height : 0,	       start : 0,       end : 0,
-	scroll : [],  top : [],        yAxis : [],
+	_obj : {},
+	type:'',
+	ipt:{},
+	data : {},
+	level: 0 ,
+	value:[],
+	tempIdx : [],
+	realIdx : [],
+	index : [],
+	dataObj : [],
+	dataArr : [],
+	warpList : [],
+	ulList : [],
+	liList : [],
+	height : 0,
+	start : 0,
+	end : 0,
+	scroll : [],
+	top : [],
+	yAxis : [],
 	flag : [],
 	//主要方法
 	go : function(param){
@@ -16,6 +30,8 @@ var scrollSelect = {
 		_obj = this;
 		_obj.level = param.level;
 		_obj.type = param.type;
+		_obj.ipt = document.getElementById(param.el).getElementsByTagName('input')[0];
+		
 		
 		if(param.type === 'address'){
 			//如果传递进来的是地址（type=address,必须传data）
@@ -26,9 +42,7 @@ var scrollSelect = {
 			});
 
 		}else if(param.type === 'calendar'){
-			$(param.elArr).each(function(index){
-				_obj.value[index] = $(this).val();
-			});
+			_obj.value[0] = _obj.ipt.value;
 			//如果传入了一个日期值：例2017-03-03
 			if(_obj.value.length == 1 && param.level != 0 && _obj.value[0] !== ''){
 				var matchArr = _obj.value[0].match(/[0-9]{1,4}/g);
@@ -39,32 +53,53 @@ var scrollSelect = {
 		}
 		
 		//大致框架
-		var $scrollSelect = $('<div class="scrollSelect"></div>'),
-			$scrollTitle = $('<div class="scrollTitle"></div>'),
-			$scrollContent = $('<div class="scrollContent level'+ _obj.level +'"></div>'),
-			$cancel = $('<span class="cancel">取消</span>'),
-			$title = $('<span class="title">请选择</span>'),
-			$submit = $('<span class="submit">确定</span>');
+		var $scrollSelect = document.createElement('div');
+			$scrollSelect.setAttribute('class','scrollSelect');
+			
+		var	$scrollTitle = document.createElement('div');
+			$scrollTitle.setAttribute('class','scrollTitle');
+			
+		var	$scrollContent = document.createElement('div');
+			$scrollContent.setAttribute('class','scrollContent level'+_obj.level)
+			
+		var	$cancel = document.createElement('span');
+			$cancel.setAttribute('class','cancel');
+			$cancel.innerHTML='取消';
+		
+		var	$title = document.createElement('span');
+			$title.setAttribute('class','title');
+			$cancel.innerHTML='请选择';
+			
+		var	$submit = document.createElement('span');
+			$submit.setAttribute('class','submit');
+			$submit.innerHTML='确定';
+			
 			//title结构
-			$scrollTitle.append($cancel,$title,$submit);
+			$scrollTitle.appendChild($cancel)
+			$scrollTitle.appendChild($title)
+			$scrollTitle.appendChild($submit)
+			
 		//根据传入的值对三种索引进行初始赋值
 		for(var i=0; i<_obj.level; i++){
 			_obj.flag[i] = false;
 			//填入数据
 			_obj.setData(i);
 			//记录重要节点
-			_obj.warpList.push($('<div class="scrollWarp"></div>'));
-			_obj.ulList.push($('<ul></ul>'));
-			_obj.warpList[i].append(_obj.ulList[i]);
+			_obj.warpList[i] = document.createElement('div');
+			_obj.warpList[i].setAttribute('class','scrollWarp')
+			_obj.ulList[i] = document.createElement('ul');
+			_obj.warpList[i].appendChild(_obj.ulList[i]);
 			_obj.setLilist(i);
-			$scrollContent.append(_obj.warpList[i]);
+			$scrollContent.appendChild(_obj.warpList[i]);
 		}
 		//将内容添加到盒子中
-		$scrollSelect.append($scrollTitle,$scrollContent)
-		$('body').append($scrollSelect)
+		$scrollSelect.appendChild($scrollTitle);
+		$scrollSelect.appendChild($scrollContent);
+		document.body.appendChild($scrollSelect);
 		
 		//开始计算尺寸
-		_obj.height = _obj.liList[0][0].height();
+		_obj.height = _obj.liList[0][0].offsetHeight;
+		
 		for(var i=0; i<_obj.level; i++){
 			_obj.top.push( (2-_obj.index[i])*_obj.height );
 			//初始化高度样式
@@ -79,21 +114,23 @@ var scrollSelect = {
 			goTouchEnd = hasTouch ? 'touchend' : 'mouseup';	
 	
 		//绑定事件--按上去
-		$(_obj.warpList).each(function(index){
-			$(this).on(goTouchStart,function(e){
-				e.preventDefault();
-				_obj.flag[index] = true;
-				_obj.start = hasTouch ? e.originalEvent.targetTouches[0].screenY : e.originalEvent.screenY;
-			})
-		})//OK
+		for(var i=0; i< _obj.warpList.length; i++){
+			_obj.warpList[i].addEventListener(goTouchStart,(function(index){
+				return function(e){
+					e.preventDefault();
+					_obj.flag[index] = true;
+					_obj.start = hasTouch ? e.targetTouches[0].screenY : e.screenY;
+				}
+			})(i))
+		}//OK
 		
 		//绑定事件--移动中
-		$('body').on(goTouchMove,function(e){
+		document.getElementsByTagName('body')[0].addEventListener(goTouchMove,function(e){
 			e.preventDefault()
 			for(var i=0; i< _obj.level; i++){
 				if(_obj.flag[i]){
 					//获取_obj.end,并重新计算_obj.scroll
-					_obj.end = hasTouch ? e.originalEvent.targetTouches[0].screenY : e.originalEvent.screenY;
+					_obj.end = hasTouch ? e.targetTouches[0].screenY : e.screenY;
 					_obj.scroll[i] = _obj.top[i] + (_obj.end - _obj.start);
 					//判断滚动尺寸在可滚动的范围内
 					if(_obj.scroll[i] <= _obj.yAxis[i][0] && _obj.scroll[i] >= _obj.yAxis[i][_obj.dataArr[i].length-1]){
@@ -101,9 +138,9 @@ var scrollSelect = {
 						_obj.index[i] = Math.abs( Math.floor((_obj.scroll[i]-_obj.height*2)/_obj.height+0.5) );
 						//判断当在一个距离中心相对距离内[模拟贴近]
 						if(_obj.scroll[i] >= _obj.yAxis[i][_obj.index[i]]-_obj.height*0.125 && _obj.scroll[i] <= _obj.yAxis[i][_obj.index[i]] + _obj.height*0.125){
-							_obj.ulList[i].css('top',_obj.yAxis[i][_obj.index[i]]);
+							_obj.ulList[i].style.top = _obj.yAxis[i][_obj.index[i]]+'px';
 						}else{
-							_obj.ulList[i].css('top',_obj.scroll[i]);
+							_obj.ulList[i].style.top = _obj.scroll[i]+'px';
 						}
 						//添加active秘wait样式
 						if(_obj.tempIdx[i] !== _obj.index[i]){
@@ -117,13 +154,13 @@ var scrollSelect = {
 		})//OK
 		
 		//绑定事件--离开
-		$(document).on(goTouchEnd,function(e){
-			e.preventDefault()
+		document.addEventListener(goTouchEnd,function(e){
+//			e.preventDefault()
 			for(var i=0; i< _obj.level; i++){
 				if(_obj.flag[i]){
 					_obj.flag[i] = false;
 					_obj.top[i] = _obj.yAxis[i][_obj.index[i]];
-					_obj.ulList[i].css('top',_obj.top[i])
+					_obj.ulList[i].style.top = _obj.top[i]+'px';
 					//判断索引是否变化再触发数据更新
 					if(_obj.realIdx[i] !== _obj.index[i]){
 						_obj.resetData(i);
@@ -134,13 +171,13 @@ var scrollSelect = {
 		})//OK
 		
 		//取消时将弹窗关闭
-		$cancel.on(goTouchStart,function(){
+		$cancel.addEventListener(goTouchStart,function(){
 			_obj.resetAll();
-			$scrollSelect.remove()
+			document.getElementsByTagName('body')[0].removeChild($scrollSelect);
 		})
 		
 		//确认按钮将数据填充进来
-		$submit.on(goTouchStart,function(){
+		$submit.addEventListener(goTouchStart,function(){
 			//根据类型不同，返回的数据也可能不同
 			var str ='';
 			if(param.type === 'address'){
@@ -148,18 +185,16 @@ var scrollSelect = {
 					str += _obj.value[index];
 					$(this).val(_obj.value[index]);
 				})
-//				$(param.el).html(str).removeClass('gray');
-				
 			}else if(param.type === 'calendar'){
 				for(var i = 0; i < _obj.value.length; i++){
+					if(parseInt(_obj.value[i]) < 10 ) _obj.value[i] = '0'+_obj.value[i];
 					str += _obj.value[i];
 				}
-//				$(param.el).html(str).removeClass('gray');
-				$(param.elArr).val(str.replace(/[年月日]/g,'-').substring(0,str.length-1));
+				_obj.ipt.value = str.replace(/[年月日]/g,'-').substring(0,str.length-1);
 			}
 			//清空对象相关数据
 			_obj.resetAll();
-			$scrollSelect.remove();
+			document.getElementsByTagName('body')[0].removeChild($scrollSelect);
 		})
 	},
 	resetAll:function(){
@@ -203,15 +238,17 @@ var scrollSelect = {
 	},
 	//添加liList
 	setLilist : function(i){
+		_obj.ulList[i].innerHTML = '';
 		_obj.liList[i] = [];
 		for(var j=0; j < _obj.dataArr[i].length; j++){
+			_obj.liList[i][j] = document.createElement('li');
 			if(_obj.type === 'address'){
-				_obj.liList[i].push($('<li>'+_obj.dataArr[i][j].address+'</li>'));
+				_obj.liList[i][j].innerHTML = _obj.dataArr[i][j].address;
 			}else if(_obj.type === 'calendar'){
-				_obj.liList[i].push($('<li>'+_obj.dataArr[i][j]+'</li>'));
+				_obj.liList[i][j].innerHTML = _obj.dataArr[i][j];
 			}
+			_obj.ulList[i].appendChild(_obj.liList[i][j]);
 		}
-		_obj.ulList[i].html(_obj.liList[i]);
 	},
 	//添加数据
 	setData : function(i){
@@ -306,7 +343,7 @@ var scrollSelect = {
 			if(_obj.liList[j][_obj.index[j]] === undefined){
 				_obj.value[j] = '无'
 			}else{
-				_obj.value[j] = $(_obj.liList[j][_obj.index[j]]).html()
+				_obj.value[j] = _obj.liList[j][_obj.index[j]].innerHTML;
 			}
 		}
 	},
@@ -368,20 +405,24 @@ var scrollSelect = {
 		//更新Y轴值
 		_obj.yAxis[i] = _obj.siteArr( _obj.dataArr[i].length , _obj.height );
 		//ul的样式
-		_obj.ulList[i].css('top',_obj.top[i]);
+		_obj.ulList[i].style.top = _obj.top[i]+'px';
 		//删除旧的样式
-		$(_obj.ulList[i]).find('.active , .wait').removeAttr('class');
+		for(var j =0; j< _obj.liList[i].length; j++){
+			if(_obj.liList[i][j].getAttribute('class')){
+				_obj.liList[i][j].removeAttribute('class');
+			}
+		}
 		//添加新的样式[如果索引存在]
 		if(_obj.liList[i][_obj.index[i]] !== undefined){
-			_obj.liList[i][_obj.index[i]].addClass('active');
+			_obj.liList[i][_obj.index[i]].setAttribute('class','active');
 		}
 		//如果索引+1存在
 		if(_obj.liList[i][_obj.index[i]+1] !== undefined){
-			_obj.liList[i][_obj.index[i]+1].addClass('wait');
+			_obj.liList[i][_obj.index[i]+1].setAttribute('class','wait');
 		}
 		//如果索引-1存在
 		if(_obj.liList[i][_obj.index[i]-1] !== undefined){
-			_obj.liList[i][_obj.index[i]-1].addClass('wait');
+			_obj.liList[i][_obj.index[i]-1].setAttribute('class','wait');
 		}
 	}
 }
